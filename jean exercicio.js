5ad2bc46b91ea1922ac58ca7ac5920315a06fd34
jeanorense-ui <jean.orense@aluno.senai.br>
2026-09-23 12:05:16 -0300

    Add files via upload

---
+++ jean exercicio.js
@@ -0,0 +1,120 @@
+//let nome = 'victor  '
+//let idade = 15
+//let altura = 1.75
+//console.log('meu nome é', nome, 'e tenho', idade, ' anos de idade. E', altura,'m de altura' )
+
+//console.log(`meu nome é ${nome}, tenho ${idade} anos de idade e ${altura}m de altura`)
+
+//let numero1 = 18
+//let numero2 = 18
+//let soma = numero1 + numero2
+//console.log (soma)
+
+//let numero1 =50
+//let numero2 =20
+//let subtraçao= numero1 - numero2
+//console.log(subtraçao)
+//let multiplicação = numero1 * numero2
+//console.log(multiplicação)
+//let divisao = numero1 / numero2
+//console.log(divisao)
+//let reasto_divisao = numero1 % numero2
+//console.log(reasto_divisao)
+
+//numero1 =17
+
+//if(numero1 % 2 ===0){
+    //console.log('seu numero é par')
+//}else{
+    //console.log('seu numero e impar')
+//}
+
+//let n1 =7
+//let n2 = '7'
+//console.log(n1 === n2)
+
+//let numero1 =51
+//if (numero1 % 5 ===0){
+   // console.log ("seu numero e divisivel")
+//}else {
+ //       console.log("seu numero não é divisivel")
+  //  }
+//let idade = 12
+//if(idade <= 12)
+//{
+ // console.log ("crianca")
+//}
+
+ // else if ( idade >= 13 && idade < 18)
+//{
+//console.log("adolecente")
+//}
+//else if (idade >= 18 && idade <= 60)
+//{
+ //   console.log ("adolto ")
+//}
+//else
+//{
+ //   console.log("idoso")
+//}
+
+//let opcao =1
+//switch(opcao){
+    //case 1: console.log ("cadastrar");
+   // break
+   // case 2: console.log("excluir");
+ //   break
+  //  case 3: console.log("editar");
+  //  break
+  //  default:console.log
+   // console.log("opçao invalida")
+//}
+
+//let numero = 14
+//let primo = true 
+//if (numero <2){
+  //  primo = false
+//}else{
+ //   for (let i = 2; i< numero; i++){
+ //       if (numero % i ===0){
+ //           primo = false
+ //           break;
+ //           }
+//}
+//}
+//console.log(primo ? `${numero} e primo`: `${numero} nao e primo`)
+
+//for ( let i = 1; i <= 5; i++){
+  //  switch (i){
+ //       case 1: 
+ //       console.log("numero 1")
+ //       break
+ //       case 3:
+ //           console.log("numero3")
+ //           break
+ //       case 5 :
+ //           console.log("numero5")
+//break;
+//default:
+  //  console.log("numero invalido")
+  //  }
+//}
+
+//for (let i =1; i <=10, i++){
+ //   console.log(i)}
+
+ //let array = [1,2,3,4,5]
+ //console.log(array)
+ //array.push(6)
+ //console.log(array)
+ //array.unshift(0)
+ //console.log(array)
+ //array.pop(3)
+ //console.log(array)
+ //let cores = ['preto', 'vermelho','branco','azul','cinza']
+ //cores [2] = 'rosa'
+ //console.log(cores)
+//}
+
+//for (let i =1; i <=10, i++){
+ //   console.log(i)}
